ea2956672922dc4f55c78886fd38272b794f10e3
Håkan Johansson <hj222je@student.lnu.se>
2021-05-30 18:57:38 +0200

    Testing ssh again

--- battleship_game/src/GameMain.java
+++ battleship_game/src/GameMain.java
@@ -1,6 +1,6 @@
 public class GameMain {
 
     public static void main (String[] args)  {
-        System.out.println("Hello World again 2.0");
+        System.out.println("Hello World again");
     }
 }
