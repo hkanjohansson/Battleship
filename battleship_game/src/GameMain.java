6ff7ab7c7348c08228e04028d55c20d349f7e5ab
Håkan Johansson <hj222je@student.lnu.se>
2021-05-30 20:06:17 +0200

    Update Hello World

--- battleship_game/src/GameMain.java
+++ battleship_game/src/GameMain.java
@@ -1,6 +1,6 @@
 public class GameMain {
 
     public static void main (String[] args)  {
-        System.out.println("Hello World again!");
+        System.out.println("Hello World again!!!");
     }
 }
