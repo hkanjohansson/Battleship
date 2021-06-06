323f2108fe1b00fa98f599dcf24fe4c32142eed8
Håkan Johansson <hj222je@student.lnu.se>
2021-06-06 12:01:41 +0200

    Updated toString

--- battleship_game/src/battleship_game/Game.java
+++ battleship_game/src/battleship_game/Game.java
@@ -1,6 +1,8 @@
 package battleship_game;
 
 import java.util.Iterator;
+import java.lang.StringBuilder;
+import java.util.List;
 
 /*
 TODO - Add all ships to a list
@@ -8,26 +10,18 @@ TODO - Make a player class
 
  */
 public class Game implements GameInterface {
-
-    /*
-    TODO - Add a constructor
-     */
     private int width;
     private int height;
+    private int numberOfShips;
+    private boolean ai;
 
-    public Game() {
-
+    public Game(boolean ai) {
+        this.width = 10;
+        this.height = 10;
+        this.numberOfShips = 5;
+        this.ai = ai;
     }
 
-    @Override
-    public int width() {
-        return 0;
-    }
-
-    @Override
-    public int height() {
-        return 0;
-    }
 
     @Override
     public Iterator<Ship> shipIter() {
@@ -37,5 +31,22 @@ public class Game implements GameInterface {
     @Override
     public void addShip(Ship s) {
 
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder gameArea = new StringBuilder();
+
+        for (int i = 0; i < width; i++) {
+            gameArea.append("-   ");
+        }
+        gameArea.append("\n");
+
+        String temp = gameArea.toString();
+
+        for (int i = 0; i < height; i++) {
+            gameArea.append(temp);
+        }
+        return gameArea.toString();
     }
 }
--- battleship_game/src/battleship_game/GameMain.java
+++ battleship_game/src/battleship_game/GameMain.java
@@ -3,6 +3,8 @@ package battleship_game;
 public class GameMain {
 
     public static void main (String[] args)  {
+        Game temp = new Game(false);
 
+        System.out.println(temp.toString());
     }
 }
