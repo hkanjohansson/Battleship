47335d1f8bc2fa32dbbdf854356a99c7e3393c0a
Håkan Johansson <hj222je@student.lnu.se>
2021-06-10 14:42:36 +0200

    Done playGame method

--- battleship_game/src/battleship_game/Game.java
+++ battleship_game/src/battleship_game/Game.java
@@ -45,7 +45,7 @@ public class Game implements GameInterface {
         Player p1 = players.get(0);
         Player p2 = players.get(1);
 
-        hits();
+
         p1.placeShip();
         p2.placeShip();
 
@@ -57,12 +57,41 @@ public class Game implements GameInterface {
         System.out.println(p1Health);
         System.out.println(p2Health);
 
-        /*
+
         while (p1Health > 0 && p2Health > 0) {
+            int[] shot = players.get(turn % 2).fireWeapon();
 
+            //int[][] weaponsFired = players.get(turn % 2).getFireArea();
+            int[][] shipsPlacement = players.get((turn + 1) % 2).getShipsArea();
 
+            if (shipsPlacement[shot[1]][shot[0]] != 0) {
+                players.get(turn % 2).setScore(1);
+                players.get((turn + 1) % 2).setTotalHealth();
+                System.out.println("Ship hit!");
+            }
+
+            System.out.println("Player " + turn % 2 + ": " + players.get(turn % 2).getScore());
+            turn++;
         }
-        */
+
+        // Player 2 will have a chance to even out the score.
+        if (turn % 2 == 0) {
+            int[] shot = players.get(turn + 1 % 2).fireWeapon();
+            int[][] shipsPlacement = players.get(turn % 2).getShipsArea();
+
+            if (shipsPlacement[shot[1]][shot[0]] != 0) {
+                players.get(turn + 1 % 2).setScore(1);
+                players.get(turn % 2).setTotalHealth();
+                System.out.println("Player " + turn + 1 % 2 + ": " + players.get(turn + 1 % 2).getScore());
+            }
+        }
+
+        for (Player p : players) {
+            if (p.getTotalHealth() > 0 ) {
+                p.setScore(p.getTotalHealth());
+            }
+        }
+
     }
 
     @Override
@@ -70,9 +99,10 @@ public class Game implements GameInterface {
 
         int[][] temp = players.get(0).getFireArea();
 
+        players.get(0).fireWeapon();
         for (int i = 0; i < players.get(0).getFireArea().length; i++) {
             for (int j = 0; j < players.get(0).getFireArea().length; j++) {
-                System.out.println(temp[i][j]);
+
 
                 /*
                 TODO - If p1 fire area[i][j] corresponds to p2 ship area[i][j] -> hit
