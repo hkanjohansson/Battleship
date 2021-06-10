0ef93b4f425e732f50ab755fc6b1a913fa33f2c3
Håkan Johansson <hj222je@student.lnu.se>
2021-06-10 11:19:36 +0200

    Started with playGame and hits methods

--- battleship_game/src/battleship_game/Game.java
+++ battleship_game/src/battleship_game/Game.java
@@ -13,7 +13,8 @@ public class Game implements GameInterface {
     private final int NUMBER_OF_PLAYERS = 2;
 
     /*
-    Should the game be initialized with two human players or one human and one AI?
+    Should the game be initialized with two human players or one human and one AI? If false then a human player 2
+    is added. If true an AI player is added.
      */
     public Game(boolean ai) {
         this.players = new ArrayList<>();
@@ -26,7 +27,7 @@ public class Game implements GameInterface {
             }
         }
 
-        System.out.println("The game is started!");
+        System.out.println("The game has started!");
     }
 
 
@@ -44,23 +45,51 @@ public class Game implements GameInterface {
         Player p1 = players.get(0);
         Player p2 = players.get(1);
 
+        hits();
         p1.placeShip();
         p2.placeShip();
 
+        int p1Health = p1.getTotalHealth();
+        int p2Health = p2.getTotalHealth();
+
         int turn = 0;
 
-        while (p1.getTotalHealth() > 0 && p2.getTotalHealth() > 0) {
-            
-        }
+        System.out.println(p1Health);
+        System.out.println(p2Health);
 
+        /*
+        while (p1Health > 0 && p2Health > 0) {
+
+
+        }
+        */
     }
 
     @Override
     public void hits() {
 
+        int[][] temp = players.get(0).getFireArea();
+
+        for (int i = 0; i < players.get(0).getFireArea().length; i++) {
+            for (int j = 0; j < players.get(0).getFireArea().length; j++) {
+                System.out.println(temp[i][j]);
+
+                /*
+                TODO - If p1 fire area[i][j] corresponds to p2 ship area[i][j] -> hit
+                        and vice versa.
+
+                TODO - Increase score
+
+                TODO - What type of ship
+                 */
+            }
+
+        }
     }
 
-
+    /*
+    Scoreboard representation
+     */
     @Override
     public String toString() {
         return null;
